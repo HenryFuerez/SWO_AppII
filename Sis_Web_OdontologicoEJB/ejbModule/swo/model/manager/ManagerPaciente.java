8fee30b4ae7fe8f02a6d1be9bb0a6f98a2f04f8c
luisanrrango <luisanrrango@localhost>
2019-07-03 16:04:45 -0400

    Creación de Manager para el modulo pacientes

---
+++ Sis_Web_OdontologicoEJB/ejbModule/swo/model/manager/ManagerPaciente.java
@@ -0,0 +1,73 @@
+package swo.model.manager;
+
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+
+import swo.model.entities.SwoPaciente;
+
+/**
+ * Session Bean implementation class MangerPaciente
+ */
+@Stateless
+@LocalBean
+public class ManagerPaciente {
+@PersistenceContext
+private EntityManager em;
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerPaciente() {
+        // TODO Auto-generated constructor stub
+    }
+ // Método que retorna la lista de  la tabla Pacientes
+    public List<SwoPaciente> findAllPacientes(){
+    	String consulta=("SELECT s FROM SwoPaciente s");
+    	Query q=em.createQuery(consulta,SwoPaciente.class);
+    	return q.getResultList();
+    	
+ 
+    }
+    //metodo para insertar un paciente
+    public void insertarPaciente(SwoPaciente paciente) throws Exception {
+  	  if(findSwoPacienteBycodigopaci(paciente.getCodigoPac())!=null)
+  		  throw new Exception("Ya existe el código indicado.");
+  	  em.persist(paciente);
+    }
+    // método de busqueda para el Pacientes
+    public SwoPaciente findSwoPacienteBycodigopaci(Integer codigo_pacie)
+    {
+  	  return em.find(SwoPaciente.class, codigo_pacie);
+  	  
+    }
+    
+    //métod para eliminar
+    public void eliminarPaciente(Integer codigo_pacie) {
+  	 SwoPaciente paciente=findSwoPacienteBycodigopaci(codigo_pacie);
+  	  if(paciente!=null)
+  		  em.remove(paciente);
+    }
+    
+  //para actualizar
+    public void actualizarSwoPaciente(SwoPaciente paciente) throws Exception {
+  	  SwoPaciente e=findSwoPacienteBycodigopaci(paciente.getCodigoPac());
+  	  if(e==null)
+  		  throw new Exception("No existe el Paciente con la código especificado");
+  	  e.setCedulaPac(paciente.getCedulaPac());
+  	  e.setNombrePac(paciente.getNombrePac());
+  	  e.setApellidoPac(paciente.getApellidoPac());
+  	 e.setFechaNaciPac(paciente.getFechaNaciPac());
+ 	e.setDireccionPac(paciente.getDireccionPac());
+ 	e.setTelefonoPac(paciente.getTelefonoPac());
+ 	e.setEmailPac(paciente.getEmailPac());
+ // 	e.setSwoGenero(paciente.getSwoGenero());
+  	  em.merge(e);
+    }
+}
+
